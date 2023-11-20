e4b09d54777e2effe216333a482603e4b80aa06e
Ocramio <148984935+Ocramio@users.noreply.github.com>
2023-11-20 01:59:05 -0300

    Add files via upload

---
+++ script.sql
@@ -0,0 +1,23 @@
+CREATE DATABASE db;
+CREATE TABLE super_tic_tac_toe(
+letra char,
+id int PRIMARY KEY NOT NULL AUTO_INCREMENT);
+INSERT INTO super_tic_tac_toe(id)
+VALUES(1);
+INSERT INTO super_tic_tac_toe(id)
+VALUES(2);
+INSERT INTO super_tic_tac_toe(id)
+VALUES(3);
+INSERT INTO super_tic_tac_toe(id)
+VALUES(4);
+INSERT INTO super_tic_tac_toe(id)
+VALUES(5);
+INSERT INTO super_tic_tac_toe(id)
+VALUES(6);
+INSERT INTO super_tic_tac_toe(id)
+VALUES(7);
+INSERT INTO super_tic_tac_toe(id)
+VALUES(8);
+INSERT INTO super_tic_tac_toe(id)
+VALUES(9);
+
